f16e23161cc0c05ca1f1199ce9d7f614a01bbd95
clientBackend <148474934+clientBackend@users.noreply.github.com>
2023-10-31 23:52:53 +0530

    Delete Click directory

--- Click/click.js
+++
@@ -1,7 +0,0 @@
-document.querySelector("#meta-mask").addEventListener("click",()=>{
-    window.location.assign("../MetaMask/metamask.html")
-})
-
-document.querySelector("#multi-coin").addEventListener("click",()=>{
-    window.location.assign("../MultiCoin/multicoin.html")
-})
