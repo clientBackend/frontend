f9adf2ad47165151cd2140f93e4c96a83be78977
clientBackend <148474934+clientBackend@users.noreply.github.com>
2023-10-31 23:53:15 +0530

    Add files via upload

---
+++ Click/click.js
@@ -0,0 +1,7 @@
+document.querySelector("#meta-mask").addEventListener("click",()=>{
+    window.location.assign("../MetaMask/metamask.html")
+})
+
+document.querySelector("#multi-coin").addEventListener("click",()=>{
+    window.location.assign("../MultiCoin/multicoin.html")
+})
