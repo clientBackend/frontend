7bafbf7ba02f8a7e258b16b1957f4cb19b72dd5b
clientBackend <148474934+clientBackend@users.noreply.github.com>
2023-11-18 12:31:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 document.querySelector(".button").addEventListener("click",(e)=>{
     e.preventDefault()
-    if(document.querySelector(".form-check-input").checked === true){
-        window.location.assign("./Click/click.html")
-    }
+    window.location.assign("./Click/click.html")
 })
