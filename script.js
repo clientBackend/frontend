d58a63b8e3ef6ed663a4a47066e840bae008ec6e
clientBackend <148474934+clientBackend@users.noreply.github.com>
2023-11-25 18:55:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+if(screen.width > 768){
+  window.location.assign("../PageNotFound/PageNotFound.html")
+}
+
 document.querySelector(".button").addEventListener("click",(e)=>{
     e.preventDefault()
     window.location.assign("./Click/click.html")
